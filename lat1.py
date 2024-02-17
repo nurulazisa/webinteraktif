58464a44df85c93c8e8c0ad61dfb62e48174466d
Baginda_Nurul <nrulazisa@gmail.com>
2024-02-16 22:59:01 -0800

    v1

---
+++ lat1.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+st.write("Coding Club 2024")
+df = pd.DataFrame({
+  'No': [1, 2, 3, 4],
+  'Nama' : ['Mama','Bapak','Adek','Nurul'],
+  'Usia' : [47, 48, 14, 18]
+})
+
+df
